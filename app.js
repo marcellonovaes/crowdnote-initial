06cb54e94f43dcea3db6e90c9ecf634718db9fcd
Marcello Novaes <cellonovaes@gmail.com>
2017-06-30 17:28:14 -0300

    Adicionado o metodo de fingerprint

--- app.js
+++ app.js
@@ -1,36 +1,52 @@
+
 var fs = require('fs');
 var express = require('express');
 var app = express();
 app.set('view engine', 'ejs');
 
+var ip = require('ip').address();
+var crypto = require('crypto');
+
+var l = require('./util/lifo.js');
 var f = require('./util/fifo.js');
 
+
+
+
+
 //Microtask 1
-var fifoA = new f.FIFO();
-var fifoB = new f.FIFO();
+var lifo = new f.FIFO();
+var fifo = new l.LIFO();
 var item;
 
 //TESTE
 for(var i=0; i<5; i++){
-	fifoA.push(i);
+	fifo.push(i);
 }
+lifo.setItems(fifo.getItems());
+fifo.clean();	
 
 app.get('/', function(req, res) {
   res.render('index', {title: 'Job Distribution System'});
 });
 
 app.get('/microtask1', function(req, res) {
-	var item = fifoA.pop();
-	fifoB.push(item);
 
-	if(fifoA.isEmpty()){
-		fifoA.setItems(fifoB.getItems());
-		fifoB.clean();	
+	var fingerprint = ip;
+	fingerprint += req.headers['user-agent'];
+	fingerprint = crypto.createHash('md5').update(fingerprint).digest("hex");
+
+	var item = lifo.pop();
+	fifo.push(item);
+
+	if(lifo.isEmpty()){
+		lifo.setItems(fifo.getItems());
+		fifo.clean();	
 	}
 
     	var bitmap = fs.readFileSync('data/test/'+item+'.jpg');
     	var image = new Buffer(bitmap).toString('base64');
-	res.render('microtask1', {item:item ,img: image});
+	res.render('microtask1', {fingerprint: fingerprint , item:item , img: image});
 });
 
 app.listen(80);

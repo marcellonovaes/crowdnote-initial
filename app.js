f7558072640eeba5183711860d79d058f1be619c
Marcello Novaes <cellonovaes@gmail.com>
2017-07-08 12:12:02 -0300

    resolvido problema do re-post no reload

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 
+// ---------------------- Includes and Globals ------------------------
+
 var host = 'localhost';
 var http = require('http');
 var path = require('path');
@@ -14,7 +16,6 @@ var lifos = new Array();
 var fifos = new Array();
 var Fingerprint = new util.Fingerprint();
 
-//Database - MongoDb
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/db');
@@ -30,26 +31,137 @@ var Schema = mongoose.Schema;
 var ObjectId = Schema.ObjectId;
 var Timestamp = Schema.Timestamp;
 
-var listJobs;
+
+// ---------------------  Init Functions -----------------------------
 
 compileSchemas();
 loadDatasets();
+app.use( bodyParser.json() );      
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+//-----------------------  Endpoints   -------------------------------
+
+// Homepage
 app.get('/', function(req, res) {
   res.render('index', {title: 'Job Distribution System'});
 });
 
 
-app.use( bodyParser.json() );      
+//----------------------------- Jobs ----------------------------------
 
-app.use(bodyParser.urlencoded({ extended: true })); 
+// Job 0
+var Job_0 = require('./jobs/595ab2f9aa17790e267ad712.js');
+Job_0 = new Job_0.job_595ab2f9aa17790e267ad712(Fingerprint, Microtasks, Jobs, Contributions, lifos, fifos, host);
+app.get('/jobs/0',Job_0.show);
+app.post('/jobs/0',Job_0.save);
+
+//------------------------ Aggragation --------------------------------
+
+//Job 0
+app.get('/aggregation/0',function(req, res) {
+
+var Contributions_0;
+var Contributions_1;
+
+getDataset();
+
+function getDataset(){
+	Dataset.find({},function (err, Dataset) {
+		if (err) return console.error(err);
+		getContributions_0(Dataset);
+	}).sort({_id: 1});
+}
+
+
+function getContributions_0(Dataset){
+	Contributions.find({microtask : '5956e7825d39ebf26fb71ee0'},function (err, Contributions) {
+		if (err) return console.error(err);
+		getContributions_1(Dataset,Contributions);
+	}).sort({date: 1});
+}
+
+function getContributions_1(Dataset,Contributions_0){
+	Contributions.find({microtask : '595ab37eaa17790e267ad714'},function (err, Contributions) {
+		if (err) return console.error(err);
+		processContributions(Dataset,Contributions_0,Contributions);
+	}).sort({date: 1});
+}
+
+function processContributions(Dataset,Contributions_0,Contributions_1){
+
+	var items = new Array();
+	for(var i=0; i < Dataset.length; i++){
+		items[Dataset[i]._id] = {'info':{'name':Dataset[i].name, 'start':Dataset[i].start}, contributions: new Array()};
+	}
+	
+	var contributions = new Array();
+	for(var i=0; i < Contributions_0.length; i++){
+		var item = items[Contributions_0[i].item].info;
+		var c0 = Contributions_0[i].contribution;
+		var c1 = Contributions_1[i].contribution;
+		contributions[i] = {'item':Contributions_0[i].item, 'start':item.start, 'c0':c0, 'c1':c1};
+	}
+
+	for(var i=0; i <  contributions.length; i++){
+		var contribution = contributions[i];
+		items[contribution.item].contributions.push(contribution);
+	}
+
+	var points = new Array();
+	for(var i=0; i < Dataset.length; i++){
+		 var list = items[Dataset[i]._id].contributions.sort(function(a,b) {
+							var x = parseInt(a.c0, 10);
+							var y = parseInt(b.c0, 10);
+							if(x > y) 	return 1;
+							else		return -1;
+						});
+
+		var current = new Array();
+		current.push(list[0]);
+
+		for(var j=1; j < list.length; j++){
+			var it = list[j];
+
+			if( parseFloat(it.c0) - parseFloat(current[0].c0) < 1){
+				current.push(it);
+			}else{
+				points.push(current);
+				current = new Array();
+				current.push(it);
+			}
+
+		}
+		points.push(current);
+	}
+
+	for(var i=0; i < points.length; i++){
+		console.log(points[i]);
+		console.log('---------------');
+	}
+
+
+/*
+	var values = {};
+	values.log = current;
+
+	res.render('aggregation/'+item.job, values);
+*/
+	
+}
+
+	
+
+});
+
+
+
+// ----------------------   File Services -----------------------------
 
 app.get('/scripts', function(req, res) {
 	var name = req.query.name;
 	var mime = req.query.mime;
 	var path = 'util/'+name+'.'+mime; 
 	fs.readFile(path, function (err, data){
-		//res.writeHead(200, {"Content-Type":"text/plain"});
-       		//res.end(data);
 		res.setHeader('content-type', 'text/javascript');
 		res.end(data);
    	});
@@ -69,117 +181,7 @@ app.get('/dataset', function(req, res) {
    	});
 });
 
-//Local Scope - isolated run
-app.get('/jobs/595ab2f9aa17790e267ad712',show_595ab2f9aa17790e267ad712);
-
-app.post('/jobs/595ab2f9aa17790e267ad712',save_595ab2f9aa17790e267ad712);
-
-
-//Job 0 - Microtasks 0 and 1
-function save_595ab2f9aa17790e267ad712(req, res) {
-	var item = req.body.item;
-	var fingerprint = req.body.fingerprint;
-  	var m0_id = req.body.m0_id;
-  	var m0_contrib = req.body.m0_contrib;
- 	var m1_id = req.body.m1_id;
-  	var m1_contrib = req.body.m1_contrib;
-
-	var c0 = new Contributions({'item':item , 'microtask':m0_id , 'contribution':m0_contrib, 'date': new Date(), 'fingerprint':fingerprint });
-	var c1 = new Contributions({'item':item , 'microtask':m1_id , 'contribution':m1_contrib, 'date': new Date(), 'fingerprint':fingerprint });
-
-	c0.save(function (err, m0) {if (err) return console.error(err);});
-
-	c1.save(function (err, m1) {if (err) return console.error(err);});
-
-
-	show_595ab2f9aa17790e267ad712(req, res);
-}
-
-function show_595ab2f9aa17790e267ad712(req, res) {
-
-	var id = 0;
-	var tmp = new Array();
-	var fingerprint = Fingerprint.get(req);
-
-
-	Microtasks.find({job : listJobs[0]._id},function (err, Microtasks) {
-		if (err) return console.error(err);
-
-		Contributions.find({fingerprint: fingerprint},function (err, Contributions) {
-			if (err) return console.error(err);
-
-			//Already contributed for all items
-			if(Contributions.length/2 == lifos[id].getItems().length + fifos[id].getItems().length){
-				res.render('finish', {});
-				return null;
-			}
-
-			//Find an item witch didn't recieve contribution from this user
-			var item = lifos[id].pop();
-			var did = true;
-			while(did){
-				if(lifos[id].isEmpty()){
-					var items = fifos[id].getItems().sort(function(a,b) {
-												var x = parseInt(a._id, 10);
-												var y = parseInt(b._id, 10);
-												if(x > y) 	return -1;
-												else		return 1;
-											});
-					lifos[id].setItems(items);
-					fifos[id].clean();	
-				}
-				var did = false;
-				for(var i=0; i < Contributions.length; i++){
-					if(Contributions[i].item.toString('utf-8').trim() == item._id.toString('utf-8').trim()){
-						tmp.push(item);
-						item = lifos[id].pop();
-						did = true;
-						break;
-					}
-				}
-			}
-			//empilha de novo os que ja tinham recebido contribuicao deste user
-			tmp = tmp.reverse();
-			for(var i=0; i < tmp.length; i++){
-				lifos[id].push(tmp[i]);
-			}
-			lifos[id].setItems(lifos[id].getItems());
-			fifos[id].push(item);
-
-			var values = {};
-			values.fingerprint = fingerprint;
-			var dir = item.job;
-			var name = item.name;
-			var mime = item.mime;
-			var start = item.start;
-			var stop = item.stop;
-
-			var path = 'data/'+dir+'/'+name+'-'+start+'-'+stop+'.'+mime;
-			var vbin = fs.readFileSync(path);
-			var v64 = new Buffer(vbin).toString('base64');
-			values.v64 = v64;
-
-			values.host = host;
-			values.item = item;
-			values.item_id = item._id;
-			values.job = item.job;
-			values.name = item.name;
-			values.mime = item.mime;
-			values.start = item.start;
-			values.stop = item.stop;
-
-			values.m0_id = Microtasks[0]._id;
-			values.m1_id = Microtasks[1]._id;
-
-			values.instruction = listJobs[id].instruction;
-
-			res.render('jobs/'+item.job, values);
-
-		}).sort({_id: -1});
-	});
-
-}
-
+// ------------- Database Functions ------------------------------
 
 function compileSchemas(){
 
@@ -226,8 +228,6 @@ function compileSchemas(){
 
 }
 
-
-
 function loadDatasets(){
 	Jobs.find({},function (err, Jobs) {
   		if (err) return console.error(err);
@@ -237,7 +237,6 @@ function loadDatasets(){
   			loadDataset(Jobs[i],i);
 
 		}
-		listJobs = Jobs;
 
 	}).sort({order: 1});
 }
@@ -255,5 +254,5 @@ function loadDataset(Job,id){
 	});
 }
 
-
+// Exec as sudo to run in port 80
 app.listen(80);
--- jobs/595ab2f9aa17790e267ad712.js
+++ jobs/595ab2f9aa17790e267ad712.js
@@ -1,68 +1,127 @@
 
+function job_595ab2f9aa17790e267ad712(Fingerprint,Microtasks,Jobs,Contributions, lifos, fifos, host){
 
-function job_595ab2f9aa17790e267ad712(){
+	var fs = require('fs');
+	
+	this.lifos = lifos;
+	this.fifos = fifos;
+	this.Fingerprint = Fingerprint;
+	this.Microtasks = Microtasks;
+	this.Contributions = Contributions;
+	this.host = host;
+
+	this.save = save;
+	this.show = show;
+
+	Jobs.findOne({_id:'595ab2f9aa17790e267ad712'},function (err, Jobs) {
+  		if (err) return console.error(err);
+
+		this.Job = Jobs;
+
+	}).sort({order: 1});
+
+	function save(req, res) {
+		var item = req.body.item;
+		var fingerprint = req.body.fingerprint;
+  		var m0_id = req.body.m0_id;
+  		var m0_contrib = req.body.m0_contrib;
+ 		var m1_id = req.body.m1_id;
+  		var m1_contrib = req.body.m1_contrib;
+
+		var c0 = new Contributions({'item':item , 'microtask':m0_id , 'contribution':m0_contrib, 'date': new Date(), 'fingerprint':fingerprint });
+		var c1 = new Contributions({'item':item , 'microtask':m1_id , 'contribution':m1_contrib, 'date': new Date(), 'fingerprint':fingerprint });
+
+		c0.save(function (err, m0) {if (err) return console.error(err);});
+		c1.save(function (err, m1) {if (err) return console.error(err);});
+
+		show(req, res);
+	}
+
+	function show(req, res) {
+		var id = 0;
+		var tmp = new Array();
+		var fingerprint = Fingerprint.get(req);
 
 
+		Microtasks.find({job : '595ab2f9aa17790e267ad712'},function (err, Microtasks) {
+			if (err) return console.error(err);
 
+			Contributions.find({fingerprint: fingerprint},function (err, Contributions) {
+				if (err) return console.error(err);
+
+				//Already contributed for all items
+				if(Contributions.length/2 == lifos[id].getItems().length + fifos[id].getItems().length){
+					res.render('finish', {});
+					return null;
+				}
+
+				//Find an item witch didn't recieve contribution from this user
+				var item = lifos[id].pop();
+				var did = true;
+				while(did){
+					if(lifos[id].isEmpty()){
+						var items = fifos[id].getItems().sort(function(a,b) {
+											var x = parseInt(a._id, 10);
+											var y = parseInt(b._id, 10);
+											if(x > y) 	return -1;
+											else		return 1;
+										});
+						lifos[id].setItems(items);
+						fifos[id].clean();	
+					}
+					var did = false;
+					for(var i=0; i < Contributions.length; i++){
+						if(Contributions[i].item.toString('utf-8').trim() == item._id.toString('utf-8').trim()){
+							tmp.push(item);
+							item = lifos[id].pop();
+							did = true;
+							break;
+						}
+					}
+				}
+				//empilha de novo os que ja tinham recebido contribuicao deste user
+				tmp = tmp.reverse();
+				for(var i=0; i < tmp.length; i++){
+					lifos[id].push(tmp[i]);
+				}
+				lifos[id].setItems(lifos[id].getItems());
+				fifos[id].push(item);
+
+				var values = {};
+				values.fingerprint = fingerprint;
+				var dir = item.job;
+				var name = item.name;
+				var mime = item.mime;
+				var start = item.start;
+				var stop = item.stop;
+
+				var path = 'data/'+dir+'/'+name+'-'+start+'-'+stop+'.'+mime;
+				var vbin = fs.readFileSync(path);
+				var v64 = new Buffer(vbin).toString('base64');
+				values.v64 = v64;
+
+				values.host = host;
+				values.item = item;
+				values.item_id = item._id;
+				values.job = item.job;
+				values.name = item.name;
+				values.mime = item.mime;
+				values.start = item.start;
+				values.stop = item.stop;
+
+				values.m0_id = Microtasks[0]._id;
+				values.m1_id = Microtasks[1]._id;
+
+				values.instruction = Job.instruction;
+
+				res.render('jobs/'+item.job, values);
+
+			}).sort({_id: -1});
+		});
+
+	}
 
 }
 
 module.exports.job_595ab2f9aa17790e267ad712 = job_595ab2f9aa17790e267ad712;
-
-
-function init_595ab2f9aa17790e267ad712(req, res) {
-//	console.log('ok');
-
-//Microtasks 0 and 1
-//5956e7825d39ebf26fb71ee0
-//595ab37eaa17790e267ad714
-
-/*	var id = 0;
-	var fingerprint = ip;
-	fingerprint += req.headers['user-agent'];
-	fingerprint = crypto.createHash('md5').update(fingerprint).digest("hex");
-*/
-
-	Contributions.find({fingerprint: fingerprint},function (err, Contributions) {
-		if (err) return console.error(err);
-
-		//Buscar na base de contributions pelo Fingerprint, somente IDs das imagens
-	
-
-		var item = lifos[id].pop();
-
-		//Se o item aparecer na lista acima, ele volta pra pilha (repete enquanto)
-
-
-		//Se o worker ainda nao contribuiu naquele item, segue em frente e ele vai pra fila
-		fifos[id].push(item);
-
-		if(lifos[id].isEmpty()){
-			lifos[id].setItems(fifos[id].getItems().reverse());
-			fifos[id].clean();	
-		}
-
-		var values = {};
-		values.fingerprint = fingerprint;
-		var dir = item.job;
-		var name = item.name;
-		var mime = item.mime;
-		var path = 'data/'+dir+'/'+name+'.'+mime;
-		var vbin = fs.readFileSync(path);
-		var v64 = new Buffer(vbin).toString('base64');
-		values.v64 = v64;
-		values.host = host;
-		values.item = item;
-		values.job = item.job;
-		values.name = item.name;
-		values.mime = item.mime;
-		values.start = item.start;
-		values.stop = item.stop;
-
-
-		res.render('595ab2f9aa17790e267ad712', values);
-
-	});
-
-
 
